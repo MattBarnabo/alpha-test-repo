28e469ba926af0c9041e6390cab4f87373bc5fe7
MattBarnabo <mattbarnabo@gmail.com>
2024-02-08 14:06:40 -0600

    adds answer to higher order functions questions

--- javascript-foundation-challenges.js
+++ javascript-foundation-challenges.js
@@ -193,36 +193,185 @@ console.log(lgNumIndex(indexHighestNumber));
 // Create a function that takes in an array of numbers and returns an array with all numbers multiplied by 10.
 // const arr1 = [3, 9, 15, 4, 10]
 // // output: [30, 90, 150, 40, 100]
+
+// const multiply10 = (array) => {
+//   return array.map((value) => value * 10)
+// }
+// console.log(multiply10(arr1));
+
+
+
 // Create a function that takes in an array of numbers and returns an array with all numbers divided by two.
 // const arr1 = [3, 9, 15, 4, 10]
 // // output: [1.5, 4.5, 7.5, 2, 5]
+// const divided2 = (array) => {
+//   return array.map((value) => value / 2)
+// }
+// console.log(divided2(arr1));
+
 // Create a function that takes in an array of numbers and returns an array with only odd numbers.
 // const arr2 = [2, 7, 3, 5, 8, 10, 13, -9]
 // // output: [7, 3, 5, 13, -9]
+
+// const oddNumSorter = (array) => {
+//   return array.filter((value) => value % 2 !== 0)
+// }
+// console.log(oddNumSorter(arr2));
+
 // Create a function that takes in a string of multiple words and returns an array with only the words that have an odd number of characters.
-// const pumbaa =
-//   "Hakuna Matata what a wonderful phrase Hakuna Matata ain't no passing craze"
+const pumbaa =
+  "Hakuna Matata what a wonderful phrase Hakuna Matata ain't no passing craze"
 // // output: ["a", "wonderful", "ain't", "passing", "craze"]
+
+const oddPhraseMaker = (array) => {
+  return array.split(" ").filter((value) => value.length % 2 !== 0)
+}
+console.log(oddPhraseMaker(pumbaa));
 // Create a function that takes in an array of numbers and letters and returns a string with only the letters. HINT: Use the typeof operator.
-// const comboArr = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
+const comboArr1 = [7, "n", "i", "c", 10, "e", false, "w", 3, "o", "r", "k"]
 // // output: "nicework"
+const stringSorter = (array) => {
+  return array.filter((value) => typeof value === "string").join("")
+    
+}
+console.log(stringSorter(comboArr1));
 // Create a function that takes in an array and returns an array without any false, null, 0, or blank values.
-// const filterArrayValues = [58, "", "abcd", true, null, false, 0]
+const filterArrayValues = [58, "", "abcd", true, null, false, 0]
 // // output: [58, "abcd", true]
+const arrayFilter = (array) => {
+  return array.filter((value) => value)
+}
+console.log(arrayFilter(filterArrayValues));
+
 // Create a function that takes in an array of strings and returns an array of strings with every other letter capitalized.
-// const makesWackyWords = ["simba", "nala", "zazu", "rafiki"]
+ const makesWackyWords = ["simba", "nala", "zazu", "rafiki"]
 // // output: ["sImBa", "nAlA", "zAzU", "rAfIkI"]
+// const capitalizer = (array) => {
+//   array.map((value) => {
+// return 
+//   })
+//   }
+  const lionKingMe = (array) => {
+    let newWordz = array.map((value) => {
+       let singleWord = value.split("")
+       return singleWord.map((value, index) => {
+        if(index % 2 !== 0){
+          return value.toUpperCase()
+          } else {
+            return value
+          }
+        }).join("")
+       })
+       return newWordz
+    }
+  
+  console.log(lionKingMe(makesWackyWords));
+
 // Create a function that takes in a string and returns a new string with all the vowels removed.
-// const str = "javascript is awesome"
+ const str = "javascript is awesome"
 // // output: "jvscrpt s wsm"
+// Create a function vowelBeGone
+// Inputs a "string"
+// Outputs a new "string" no vowels
+// Change "string" into [array] stringToArray
+// Change "string" into [array]
+// Iterate through [array] 
+// If? else: statement to remove vowels
+// Return  [array]
+// Change array into string
+// log and test function
+
+// const vowelBeGone = (string) => {
+//   let stringToArray = string.split("")
+//   let noVowel = stringToArray.filter((value) => {
+//     if (value !== "a" && value !== "e" && value !== "i" && value !== "o" && value !== "u"){
+//       return value
+//     }
+//     })
+//     return noVowel.join("")
+//   }
+
+// console.log(vowelBeGone(str));
+
+// const vowelBeGone = (string) => {
+//   let stringToArray = string.split("")
+//   let noVowel = stringToArray.filter((value) => {
+//    if( value.includes("a", "e", "i", "o", "u") !== true){
+//       return value
+//     }
+//     })
+//     return noVowel.join("")
+//   }
+
+// console.log(vowelBeGone(str));
+
+const vowelBeGone = (string) => {
+  let stringToArray = string.split("")
+  return stringToArray.filter(value => !value.includes("a", "e", "i", "o", "u")).join("")
+  }
+
+console.log(vowelBeGone(str));
+
+
+
+
 // Create a function that takes in a string containing numbers and returns an array with each number at its own index and converted to the data type of number.
-// const stringOfNumbers = "4574328"
+const stringOfNumbers = "4574328"
 // // output: [4, 5, 7, 4, 3, 2, 8]
+
+// Create function that inputs a string of numbers
+// Outputs an array of numbers
+// Convert string to array of individual numbers
+// iterate through array
+// convert array of "strings" to Numbers
+// Return array of numbers containing their own index
+const numberMaker = (stringOfNums) => {
+  return stringOfNums.split("").map(value => +value)
+
+}
+console.log(numberMaker(stringOfNumbers));
+
+
+
+// const numberMaker = (stringOfNums) => {
+//   return stringOfNums.split("").map(Number)
+
+// }
+// console.log(numberMaker(stringOfNumbers));
+
+
+
 // 🏔 Stretch Goals
 // Create a function that takes in two arrays as arguments returns one array with no duplicate values.
-// const arr1 = [3, 7, 10, 5, 4, 3]
-// const arr2 = [7, 8, 2, 1, 5, 4]
+
+//  create function that inputs 2 seperate arrays
+// Create var to concat arrays
+// Return a filtered array comparing first instance of value to the first index location
+// Output an array without duplicate values
+
+ const arr1 = [3, 7, 10, 5, 4, 3]
+ const arr2 = [7, 8, 2, 1, 5, 4]
 // // output: [3, 7, 10, 5, 4, 8, 2, 1]
+const noDuplicates = (array1, array2) => {
+  let twoArray = array1.concat(array2)
+  return twoArray.filter((value, index) => twoArray.indexOf(value) === index)
+} 
+console.log(noDuplicates(arr1, arr2));
+
+
+
+
 // Create a function that takes in an array of mixed data types and returns the first value that is a string. HINT: look into the JavaScript .find() method.
-// const allTheData = [7, null, true, 2, "yo!", false, 4, "hello!"]
+ const allTheData = [7, null, true, 2, "yo!", false, 4, "hello!"]
 // // output: "yo!"
+
+// CREATE a function 
+// INPUT a mixed data array
+// FIND the first instance of a string in the array
+// RETURN the string
+
+const stringFinder = (array) => {
+return array.find(element => typeof element === "string")
+}
+console.log(stringFinder(allTheData));
+
